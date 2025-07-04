b81dcaa91083b44c30f4aa5d9c67aef4ac02551e
DarwinAroca <darwinaroca@hotmail.com>
2025-07-03 22:22:48 -0500

    finalizacion

--- src/relaciones/Main.java
+++ src/relaciones/Main.java
@@ -3,8 +3,8 @@ package relaciones;
 public class Main {
     public static void main(String[] args) {
         Diagnostico diag = new Diagnostico(
-                "Infección en la piel",
-                "Administrar antibiótico tópico durante 10 días y monitorear reacción."
+                "Infección  ocular",
+                "Administrar antibiótico en gotas durante 15 días y monitorear reacción."
         );
 
         Consulta consulta = new Consulta("C001", "2025-07-01", diag);
